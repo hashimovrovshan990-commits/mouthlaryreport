c71a87262573bdc8d57cfcb5be4bf543e06a3bd8
hashimovrovshan990-commits <hashimovrovshan990@gmail.com>
2026-03-11 01:27:00 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,9 @@
-BOT_TOKEN = "8258447024:AAG2qla6xWlSMPhOdX3klBGVhnP5fb2W2Pk"
-ADMIN_ID = 680001144
+import os
+
+BOT_TOKEN = os.getenv("8258447024:AAG2qla6xWlSMPhOdX3klBGVhnP5fb2W2Pk")
+ADMIN_ID = int(os.getenv("680001144", "0"))
+
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN не задан в переменных окружения")
+if ADMIN_ID == 0:
+    raise ValueError("❌ ADMIN_ID не задан в переменных окружения")
